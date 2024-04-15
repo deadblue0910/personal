d76e1bac52ddd3b90a31fa324d642aaa20516985
deadblue0910 <48860218+deadblue0910@users.noreply.github.com>
2024-04-15 16:20:34 +0800

    Update emby.sh

--- emby.sh
+++ emby.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+read -p "请输入 Emby 容器名称:" name
+
 #破解premiere
-rm -f Emby.Notifications.dll
 wget --no-check-certificate https://service.hexsen.com/emby/crack49/Emby.Notifications.dll
-rm -f Emby.Web.dll
+docker cp ./Emby.Notifications.dll $name:/system/
 wget --no-check-certificate https://service.hexsen.com/emby/crack49/Emby.Web.dll
-rm -f MediaBrowser.Model.dll
+docker cp ./Emby.Web.dll $name:/system/
 wget --no-check-certificate https://service.hexsen.com/emby/crack49/MediaBrowser.Model.dll
+docker cp ./MediaBrowser.Model.dll $name:/system/
 echo "核心开心完成..."lient/
-cd dashboard-ui/embypremiere/
-rm -f embypremiere.js
 wget --no-check-certificate https://service.hexsen.com/emby/crack49/dashboard-ui/embypremiere/embypremiere.js
-chmod 644 embypremiere.js
+docker cp ./Emby.Web.dll $name:/system/dashboard-ui/embypremiere/
+docker exec -it $name chmod 644 /system/dashboard-ui/embypremiere/embypremiere.js
 echo "web 开心完成..."
-cd ../../
-rm -f Emby.Server.Implementations.dll
-wget --no-check-certificate https://service.hexsen.com/emby/crack49/Emby.Server.Implementations.dll
+wget --no-check-certificate https://service.hexsen.com/emby/crack49/
+docker cp ./Emby.Server.Implementations.dll $name:/system/
 echo "Implementations 替换认证..."
-chmod 644 Emby.Web.dll Emby.Server.Implementations.dll MediaBrowser.Model.dll Emby.Notifications.dll
+docker exec -it $name chmod 644 Emby.Web.dll Emby.Server.Implementations.dll MediaBrowser.Model.dll Emby.Notifications.dll
+rm -f Emby.Notifications.dll
+rm -f Emby.Web.dll
+rm -f MediaBrowser.Model.dll
+rm -f embypremiere.js
+rm -f Emby.Server.Implementations.dll
 
